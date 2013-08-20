7e83819ee10463cd32f6c932e477f742405f6631
Arpit Mathur <mathur.arpit@gmail.com>
2013-08-19 22:09:37 -0400

    Added icon files, changed executable name

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@
 int main(){
 	ofAppGlutWindow window; // create a window
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1400, 1100, OF_FULLSCREEN);
+	ofSetupOpenGL(&window, 1400, 900, OF_WINDOW);
 	ofRunApp(new testApp()); // start the app
 }
